70980058b627a1b1a5c183df6285202023b9b845
Sivan wolberg <sivan@wolberg.pro>
2024-10-09 15:40:10 +0300

    Add rate limiting for API requests in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use RateLimiter;
+use Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,4 +27,5 @@ class AppServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(1000)->by($request->user()?->id ?: $request->ip());
         });
+    }
 }
