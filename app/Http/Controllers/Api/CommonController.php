2e6c414754df98893245755595f92b0eab6e356f
Sivan wolberg <sivan@wolberg.pro>
2024-10-19 02:03:22 +0300

    Add supported languages endpoint: implement getSupportedLanguages method in CommonController, update routes for language support, and enhance logging for requests

--- app/Http/Controllers/Api/CommonController.php
+++ app/Http/Controllers/Api/CommonController.php
@@ -5,22 +5,30 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Traits\ApiResponseTrait;
 use BaseApiController;
+use DB;
 use Illuminate\Http\Request;
 use Log;
 
 class CommonController extends BaseApiController
 {
-    public function get_countries()
+    public function getCountries()
     {
         Log::info('Countries request received');
         $countries = countries();
         return $this->successResponse($countries);
     }
 
-    public function get_platform_settings()
+    public function getPlatformSettings()
     {
         Log::info('Platform settings request received');
         $settings = new \stdClass();
         return $this->successResponse($settings);
+    }
+
+    public function getSupportedLanguages()
+    {
+        Log::info('Supported languages request received');
+        $languages = DB::table('languages')->where('is_supported', '=', true)->get();
+        return $this->successResponse($languages);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+Route::prefix('common')->group(function () {
+    Route::get('/supported/languages', 'CommonController@getSupportedLanguages');
+    Route::get('/platform/settings', 'CommonController@getPlatformSettings');
+    Route::get('/countries', 'CommonController@getCountries');
+});
 Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::prefix('user')->group(function () {
+        Route::get('/me', 'UserController@me');
+        Route::get('/{id}', 'UserController@profile');
+        Route::put('/{id}', 'UserController@update');
+        Route::put('/{id}/languages', 'UserController@updateLanguages');
+    });
     Route::group(['prefix' => 'business'], function () {
         Route::get('/list', 'BusinessController@index');
         Route::get('/{id}', 'BusinessController@show');
